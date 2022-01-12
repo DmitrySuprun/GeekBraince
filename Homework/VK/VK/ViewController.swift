0f46a6bf4d78104a1f413838679f8f6d6292af73
Dmitry <suprun1212@gmail.com>
2022-01-12 22:33:48 +0300

    add weather

--- Homework/VK/VK/ViewController.swift
+++ Homework/VK/VK/ViewController.swift
@@ -1,19 +1,19 @@
 //
 //  ViewController.swift
-//  Storyboard
+//  VK
 //
-//  Created by Дмитрий Супрун on 3.01.22.
+//  Created by Дмитрий Супрун on 9.01.22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
 
 }
 
---
+++ Homework/Wheather/Wheather/LoginFormController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginFormController.swift
+//  Wheather
+//
+//  Created by Дмитрий Супрун on 5.01.22.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+//    @IBAction func loginButtonPressed(_ sender: Any) {
+//            // Получаем текст логина
+//            let login = loginInput.text!
+//            // Получаем текст-пароль
+//            let password = passwordInput.text!
+//
+//            // Проверяем, верны ли они
+//            if login == "admin" && password == "123456" {
+//                print("успешная авторизация")
+//            } else {
+//                print("неуспешная авторизация")
+//            }
+//        }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
