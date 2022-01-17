64008a67b4be53e7c8b71a6da07bff91094d3d68
Dmitry <suprun1212@gmail.com>
2022-01-17 17:14:35 +0300

    UINavigationController. Segue.

--- Homework/VK/VK/ViewController.swift
+++ Homework/VK/VK/ViewController.swift
@@ -13,7 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func enterButton(_ sender: UIButton) {
+        if login.text == "admin" && password.text == "12345" {
+            
+        } else {
+            let alertWrongPassword = UIAlertController(title: "Ошибка", message: "Неверный пароль", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "Ok", style: .default, handler: {_ in 
+                self.login.text = ""
+                self.password.text = ""
+            })
+            alertWrongPassword.addAction(okButton)
+            present(alertWrongPassword, animated: true, completion: nil)
+        }
+    }
+    
 
-
+    @IBAction func showPassword(_ sender: UIButton) {
+        password.isSecureTextEntry = false
+        
+    }
+    
 }
 
